145e7a2d6132189987d956ae716012d18d07914a
Shelby Wilson <shelbyleanne22@gmail.com>
2025-08-07 20:25:59 -0400

    updated look of descriptions

--- shelby-wilson/app/reactFun/hookVisualizer/page.tsx
+++ shelby-wilson/app/reactFun/hookVisualizer/page.tsx
@@ -5,7 +5,7 @@ export default function HookMeUp() {
     return (
         <div className="p-8 max-w-4xl mx-auto">
             <h1 className="text-3xl font-bold mb-6 text-center">🪝 Hook Visualizer!</h1>
-            <p className="text-xl text-center p-4 m-2">The Hook Visualizer demonstrates the use of React hooks that help manipulate state.</p>
+            <p className="text-md text-center md:text-left p-4 m-2 italic">The Hook Visualizer demonstrates the use of React hooks that help manipulate state.</p>
             <HookMeUpVisualizer />
         </div>
     )
--- shelby-wilson/app/reactFun/moodTracker/page.tsx
+++ shelby-wilson/app/reactFun/moodTracker/page.tsx
@@ -5,7 +5,7 @@ export default function MoodTracker() {
     return (
         <div className="p-8 max-w-xl mx-auto text-center">
             <h1 className='text-3xl font-bold mb-4'>Mood Tracker</h1>
-            <p className="text-xl text-center p-4 m-2">The Mood Tracker uses localStorage and React Context to track selected moods.</p>
+            <p className="text-md text-center md:text-left p-4 m-2 italic">The Mood Tracker uses localStorage and React Context to track selected moods.</p>
             <EmojiMoodTracker />
         </div>
     )
