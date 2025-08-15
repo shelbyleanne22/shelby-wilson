e400c361923165b8057b306a2e56a86115c180ed
Shelby Wilson <shelbyleanne22@gmail.com>
2025-08-15 15:13:23 -0400

    small formatting changes

--- shelby-wilson/components/resume/Experience.tsx
+++ shelby-wilson/components/resume/Experience.tsx
@@ -4,12 +4,18 @@ export default function Experience() {
     return (
         <section>
             {resumeData.experience.map((job, idx) => (
-                <div key={idx} className="mb-6">
-                    <h3 className="text-lg font-semibold dark:text-teal-500">{job.company}</h3>
-                    <span className="text-sm text-gray-500 dark:text-white">{job.startDate} - {job.endDate}</span>
-                    <p className="mt-2 text-gray-700 italic dark:text-white">{job.position}</p>
+                <div key={idx} className="mb-6 p-4 shadow-sm rounded-lg bg-white dark:bg-gray-800">
+                    <h3 className="text-lg font-semibold dark:text-teal-500">
+                        {job.company}
+                    </h3>
+                    <span className="text-sm text-gray-500 dark:text-white">
+                        {job.startDate} - {job.endDate}
+                    </span>
+                    <p className="mt-2 text-gray-700 italic dark:text-white">
+                        {job.position}
+                    </p>
                     <div className="flex flex-wrap gap-2 mt-2">
-                        <ul className="list list-disc">
+                        <ul className="list-none pl-4 mt-2 space-y-1">
                             {job.responsibilities.map((responsibility, idx) => (
                                 <li key={idx}>{responsibility}</li>
                             ))}
