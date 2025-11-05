cb563ea0f33f49994ee042684901e4520ff20699
Shelby Wilson <shelbyleanne22@gmail.com>
2025-11-05 06:41:17 -0600

    fix factorial

--- shelby-wilson/components/demos/UseMemoDemo.tsx
+++ shelby-wilson/components/demos/UseMemoDemo.tsx
@@ -2,11 +2,11 @@
 import { useMemo, useState } from "react";
 
 export default function useMemoDemo() {
-    const [number, setNumber] = useState(1);
+    const [number, setNumber] = useState<number | "">("");
 
     const factorial = useMemo(() => {
         const calc = (n: number): number => (n <= 1 ? 1 : n * calc(n - 1));
-        return calc(number);
+        return typeof number === "number" ? calc(number) : "";
     }, [number]);
 
     return (
@@ -14,11 +14,14 @@ export default function useMemoDemo() {
             <input
                 type="number"
                 value={number}
-                onChange={(e) => setNumber(Number(e.target.value))}
+                onChange={(e) => {
+                    let value = e.target.value;
+                    setNumber(value === "" ? "" : Number(value))
+                }}
                 className="border px-4 py-2 rounded mr-4 mb-2 w-full"
                 min={0}
             />
-            <p className="w-full px-4 py-2 rounded font-bold">Factorial: {factorial}</p>
+            <p className="w-full px-4 py-2 rounded font-bold">Factorial: {number === "" ? "-" : factorial}</p>
             <p>UseMemo remembers the result of a slow calculation so if nothing changes
                 it uses the same result instead of doing the work again.
             </p>
